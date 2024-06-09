40c279a9400e6f16e2d2f7d91f29bbefc494741e
Umiterr <37944964+Umiterr@users.noreply.github.com>
2024-06-09 00:47:52 -0600

    add update, delete, and get URL functions

--- src/types/services/urls.ts
+++ src/types/services/urls.ts
@@ -1,6 +1,6 @@
 import knex from "knex";
 
-export const createShortUR = async (
+export const createShortURL = async (
   body: { url: string; id?: string },
   user_id: number
 ) => {
@@ -25,7 +25,53 @@ export const createShortUR = async (
 export const resolveURL = async (id: string) => {
   const url = await knex("urls").where({ id }).select(["url"]).first();
   if (!url) {
-    throw new Error("The is is not valid");
+    throw new Error("The id is not valid");
   }
   return url.url;
+};
+
+export const updateURL = async (
+  id: string,
+  body: { url: string },
+  user_id: number
+) => {
+  if (!body.url) {
+    throw new Error("URL is required");
+  }
+  const url = await knex("urls").where({ id }).select(["user_id"]).first();
+  if (!url) {
+    throw new Error("URL not found");
+  }
+  if (url.user_id !== user_id) {
+    throw new Error("You are not allowed to update this URL");
+  }
+  const results = await knex("urls")
+    .where({ id })
+    .update({ url: body.url }, "*");
+  return results[0];
+};
+
+export const deleteURL = async (id: string, user_id: number) => {
+  const url = await knex("urls").where({ id }).select(["user_id"]).first();
+  if (!url) {
+    throw new Error("URL not found");
+  }
+  if (url.user_id !== user_id) {
+    throw new Error("You are not allowed to update this URL");
+  }
+
+  await knex("urls").where({ id }).delete();
+  return true;
+};
+
+export const getURLS = async (
+  user_id: number,
+  limit: number,
+  offset: number
+) => {
+  const results = await knex("urls")
+    .where({ user_id })
+    .limit(limit || 15)
+    .offset(offset || 0);
+  return results;
 };
